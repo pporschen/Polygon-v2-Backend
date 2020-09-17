12298c753d6e30616a32b9087a8abfbc189668de
Patrick <accounts@pp83.de>
2020-09-18 01:51:20 +0200

    .env

--- database/client.js
+++ database/client.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://pporschen:h8NzZuVjc3Jlkh7w@cluster0.owggm.mongodb.net/polygo?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
